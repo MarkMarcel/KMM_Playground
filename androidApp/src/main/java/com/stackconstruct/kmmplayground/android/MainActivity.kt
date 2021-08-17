8bb4cce6cb3d1c98000d98574511a5b78905664e
Mark Amoako Marcel <freelancemarcel18@gmail.com>
2021-08-17 09:04:15 +0000

    switching to explore Google Maps using layout files,Activities and Views

--- androidApp/src/main/java/com/stackconstruct/kmmplayground/android/MainActivity.kt
+++ androidApp/src/main/java/com/stackconstruct/kmmplayground/android/MainActivity.kt
@@ -23,9 +23,13 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.MapView
+import com.google.android.libraries.maps.OnMapReadyCallback
+import com.google.android.libraries.maps.SupportMapFragment
 import com.stackconstruct.kmmplayground.Greeting
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -33,143 +37,38 @@ import kotlinx.coroutines.launch
 
 data class OSAndVersion(val os: String, val version: String)
 
-@Composable
-fun OSAndVersion(osAndVersion: OSAndVersion) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_baseline_android_24),
-            contentDescription = "Android",
-            modifier = Modifier
-                .size(48.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.primary)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Column {
-            Text(text = osAndVersion.os, color = MaterialTheme.colors.primary)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = osAndVersion.version)
-        }
-    }
-}
 
 fun greet(): String {
     return Greeting().greeting()
 }
-@Composable
-fun rememberMapViewWithLifecycle(): MapView {
-    val context = LocalContext.current
-    val mapView = remember {
-        MapView(context).apply {
-            id = R.id.map
-        }
-    }
 
-    // Makes MapView follow the lifecycle of this composable
-    val lifecycleObserver = rememberMapLifecycleObserver(mapView)
-    val lifecycle = LocalLifecycleOwner.current.lifecycle
-    DisposableEffect(lifecycle) {
-        lifecycle.addObserver(lifecycleObserver)
-        onDispose {
-            lifecycle.removeObserver(lifecycleObserver)
-        }
-    }
-
-    return mapView
-}
-
-@Composable
-fun rememberMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
-    remember(mapView) {
-        LifecycleEventObserver { _, event ->
-            when (event) {
-                Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
-                Lifecycle.Event.ON_START -> mapView.onStart()
-                Lifecycle.Event.ON_RESUME -> mapView.onResume()
-                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
-                Lifecycle.Event.ON_STOP -> mapView.onStop()
-                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
-                else -> throw IllegalStateException()
-            }
-        }
-    }
+//Do not forget handling lifecycle events during the use of a feature
+//Create lifecycle aware components?
+//when updating a polyvector - add update type to points (new,modified,deleted)
 class MainActivity : AppCompatActivity() {
-    //lateinit var mapView: MapView
+
+    private object MapState : OnMapReadyCallback {
+        override fun onMapReady(map: GoogleMap?) {
+            map?.uiSettings?.setAllGesturesEnabled(true)
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val osAndVersion = greet().split(" ")
-        setContent {
-            val context = LocalContext.current
-            val mapView = MapView(context)
-            mapView.onCreate(Bundle())
-            /*val observer = LifecycleEventObserver { _, event ->
-                when (event) {
-                    Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
-                    Lifecycle.Event.ON_START -> mapView.onStart()
-                    Lifecycle.Event.ON_RESUME -> mapView.onResume()
-                    Lifecycle.Event.ON_PAUSE -> mapView.onPause()
-                    Lifecycle.Event.ON_STOP -> mapView.onStop()
-                    Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
-                    else -> throw IllegalStateException()
-                }
-            }
-            LocalLifecycleOwner.current.lifecycle.addObserver(observer)*/
-            MaterialTheme {
-                Column(modifier = Modifier.fillMaxHeight()) {
-                    OSAndVersion(osAndVersion = OSAndVersion(osAndVersion[0],osAndVersion[1]))
-                    AndroidView({MapView(context).apply { onCreate(Bundle()) }}){
-                        mapView.getMapAsync {
-                            it.uiSettings.setAllGesturesEnabled(true)
-                        }
-                    }
-                }
-            }
+        if (savedInstanceState == null) {
+            setContentView(FragmentContainerView(this))
+            addMapFragment()
         }
     }
 
-    /*override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        val mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY) ?: Bundle().also {
-            outState.putBundle(MAPVIEW_BUNDLE_KEY, it)
-        }
-        mapView.onSaveInstanceState(mapViewBundle)
+    private fun addMapFragment() {
+        val mapView = SupportMapFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .add(mapView, "map")
+            .commitNow()
+        mapView.getMapAsync(MapState)
     }
 
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onMapReady(map: GoogleMap) {
-        map.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker"))
-    }
-
-    override fun onPause() {
-        mapView.onPause()
-        super.onPause()
-    }
-
-    override fun onDestroy() {
-        mapView.onDestroy()
-        super.onDestroy()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
-    }
-
-    companion object {
-        private const val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
-    }*/
 }
